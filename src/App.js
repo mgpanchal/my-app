8fbb77ab1ddea6b77ae382dbf501b11835d4eac6
Mangesh Panchal <mgpanchal007@outlook.com>
2024-08-06 18:20:59 +0530

    production changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>Hello, Mangesh Panchal</h1>
+       <p>This is my first producation app.</p>
       </header>
     </div>
   );
