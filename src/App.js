4f10d00671322354df906e34f4ab063f8599e11c
Mangesh Panchal <mgpanchal007@outlook.com>
2024-08-06 18:23:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
        <h1>Hello, Mangesh Panchal</h1>
        <p>This is my first producation app.</p>
+       <h3>Hello</h3>
       </header>
     </div>
   );
